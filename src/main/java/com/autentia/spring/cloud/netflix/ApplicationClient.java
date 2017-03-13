3fb9e178ac307ddc8710bc31e4ac05975357cac3
Jose Mangialomini <jmangialomini@autentia.com>
2017-03-13 09:46:04 +0100

    pom and properties updates

--- src/main/java/com/autentia/spring/cloud/netflix/ApplicationClient.java
+++ src/main/java/com/autentia/spring/cloud/netflix/ApplicationClient.java
@@ -1,27 +1,12 @@
 package com.autentia.spring.cloud.netflix;
 
 import org.springframework.boot.SpringApplication;
-import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
-import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
-import org.springframework.context.annotation.Configuration;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 
-@Configuration
-@EnableAutoConfiguration
-@EnableEurekaClient
-@RestController
+@SpringBootApplication
+@EnableDiscoveryClient
 public class ApplicationClient {
-
-	  @RequestMapping("/")
-	  public String home() {
-	    return "Autentia: REST Client - OK";
-	  }
-	  
-	  @RequestMapping("/info")
-	  public String info() {
-	    return home();
-	  }
 
 	  public static void main(String[] args) {
 	    SpringApplication.run(ApplicationClient.class, args);
---
+++ src/test/java/com/autentia/spring/cloud/netflix/ApplicationClientTest.java
@@ -0,0 +1,16 @@
+package com.autentia.spring.cloud.netflix;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class ApplicationClientTest{
+
+    @Test
+    public void contextLoads() throws Exception {
+    }
+
+}
