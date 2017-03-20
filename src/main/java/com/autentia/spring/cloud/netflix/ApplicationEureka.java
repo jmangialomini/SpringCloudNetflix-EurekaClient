ee0d85da4f14fcea8d1295012f4fc71d781b5492
Jose Mangialomini <jmangialomini@autentia.com>
2017-03-20 21:42:17 +0100

    Main class name update

--- src/main/java/com/autentia/spring/cloud/netflix/ApplicationEureka.java
+++ src/main/java/com/autentia/spring/cloud/netflix/ApplicationEureka.java
@@ -6,10 +6,10 @@ import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 
 @SpringBootApplication
 @EnableDiscoveryClient
-public class ApplicationClient {
+public class ApplicationEureka {
+ 
+	public static void main(String[] args) {
+     SpringApplication.run(ApplicationEureka.class, args);
+    }
 
-	  public static void main(String[] args) {
-	    SpringApplication.run(ApplicationClient.class, args);
-	  }
-
-	}
+}
